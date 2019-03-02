2e0a4bd51763f8f66b68e16862a988b1bf6e06fd
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-01 21:34:11 -0500

    Make a Delegate

--- c0732134-Week08/Program.cs
+++ c0732134-Week08/Program.cs
@@ -8,8 +8,22 @@ namespace c0732134_Week08
 {
     class Program
     {
+
+        public delegate string MyDelegate(int argument1, int argument2);            // A type of method, which can be paeesd around just like a variable
+
+        static string func1(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        static string func2(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+
         static void Main(string[] args)
         {
+            
         }
     }
 }
